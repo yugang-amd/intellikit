311cd54a0899b4213fb17a7c47fba34db4866075
yugang-amd <yugang.wang@amd.com>
2026-07-08 15:25:22 -0400

    exclude .mdx

--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -5,7 +5,7 @@ import { glob } from "astro/loaders";
 export const collections = {
   docs: defineCollection({
     loader: glob({
-      pattern: ["**/[^_]*.{md,mdx}", "!node_modules/**"],
+      pattern: "{*.md,*.mdx,getting-started/*.md,getting-started/*.mdx,how-to/*.md,how-to/*.mdx,tools/*.md,tools/*.mdx}",
       base: new URL("../../", import.meta.url),
     }),
     schema: docsSchema(),
